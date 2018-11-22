37631504709248097e69831291900d0b86db0523
Joshua Robinson <jbrobin2@gmail.com>
2018-11-21 23:49:50 -0500

    latest

--- src/index.js
+++ src/index.js
@@ -1,11 +1,28 @@
-exports.handler = (event, context, callback) => {
+import fetch from 'node-fetch';
+
+exports.handler = async (event, context, callback) => {
+  const request = fetch('https://google.com', {
+    method: 'HEAD'
+  });
+
+  let data;
+  try {
+    const response = await request;
+
+    data = {
+      url: response.url,
+      status: response.status,
+      statusText: response.statusText
+    };
+  } catch (e) {
+    console.log(e);
+  }
+
   return callback(null, {
     statusCode: 200,
     header: {
       'Content-Type': 'application/json'
     },
-    body: JSON.stringify({
-      "foo": "bar"
-    })
+    body: JSON.stringify(data)
   });
 }
