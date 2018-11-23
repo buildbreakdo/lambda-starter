ad450ba596928883f82e5f4bb593e7b5538936c6
Joshua Robinson <jbrobin2@gmail.com>
2018-11-23 09:07:29 -0500

    fix test script; explicitly state babel plugins in webpack to ensure they are always used (especially in monorepos)

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,17 @@ module.exports = {
       {
         test: /\.(mjs|js|jsx)$/,
         exclude: /node_modules/,
-        loader: 'babel-loader'
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            '@babel/preset-env',
+            {
+              plugins: [
+                '@babel/plugin-proposal-class-properties'
+              ]
+            }
+          ]
+        },
       }
     ],
   }
