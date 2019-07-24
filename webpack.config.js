dd8e65f791cb4804600b7e4d233e2bd486d8aee0
Joshua Robinson <jbrobin2@gmail.com>
2019-07-24 03:28:20 -0400

    webpack.config.js: add moment.js locale handling; comment why pg-native is excluded

--- webpack.config.js
+++ webpack.config.js
@@ -21,11 +21,22 @@ module.exports = {
     libraryTarget: 'commonjs2'
   },
   plugins: [
+    // Node-Postgres.js is the cononical SQL postgres library; when bundling
+    // pg-native must be ignored to build
+    // See: https://github.com/serverless-heaven/serverless-webpack/issues/78
     new webpack.IgnorePlugin(/^pg-native$/),
+
     new webpack.DefinePlugin({
       'process.env.BROWSER': false,
       __DEV__: process.env.NODE_ENV !== 'production',
     }),
+    //
+    // Moment.js is an extremely popular library that bundles large locale files
+    // by default due to how Webpack interprets its code. This is a practical
+    // solution that requires the user to opt into importing specific locales.
+    // https://github.com/jmblog/how-to-optimize-momentjs-with-webpack
+    // You can remove this if you don't use Moment.js:
+    new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
   ],
   module: {
     rules: [
